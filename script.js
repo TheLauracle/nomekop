48bf73854c26305ac1167bd2e5bc12a4da318e67
Lauren <flying.cookies@yahoo.com>
2016-04-04 15:04:29 -0500

    stop trying to rotate sidead
    it's not going to work

--- script.js
+++ script.js
@@ -3,11 +3,4 @@ window.onload = function(){
 	var random = Math.random() * 20;
 	console.log(random);
 	document.getElementById("sidead").style.backgroundImage = "url('catbg1.png')";
-	
-	//set widthish of sideways text div to be the height of the window
-	var div = document.getElementById('sidead');
-    var height = div.clientHeight;
-    console.log(height);
-    document.getElementById('sideadcontent').style.width = height + 'px';
-
 }
