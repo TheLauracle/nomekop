2db6b4ea0f0b7a9b5207fc2cdc05fd72b6a4406b
Lauren <flying.cookies@yahoo.com>
2016-04-06 15:52:34 -0500

    add userMode and recognize 'play game' button
    along with some miscellaneous edits such as an attempt at proper
    capitalization, the game.js should now check to see whether the player
    is looking at the menu, and should be able to tell the difference
    between the play game button and the rest of the canvas.

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ var context = document.getElementById("thegame").getContext('2d');
 context.canvas.width = 700;
 context.canvas.height = 600;
 
+//what to draw and how to treat events; ideas being menu, intro, overworld
+var userMode = 'menu';
 
 
 //** -------------- ------------- -------------- **
@@ -12,17 +14,24 @@ context.canvas.height = 600;
 //** -------------- ------------- -------------- **
 
 //function to draw the background
-function drawbackground(){
-	var bwgradient = context.createLinearGradient(0,0,context.canvas.width, context.canvas.height);
-	bwgradient.addColorStop(0, "red");
-	bwgradient.addColorStop(1, "white");
-	context.fillStyle = bwgradient;
-	context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+function drawBackground(){
+	switch(userMode) {
+		case 'menu':
+			var menugradient = context.createLinearGradient(0,0,context.canvas.width, context.canvas.height);
+			menugradient.addColorStop(0, "red");
+			menugradient.addColorStop(1, "white");
+			context.fillStyle = menugradient;
+			context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+			break;
+		default:
+			break;
+	}
 }
 
 //TBA - add menu buttons
-function drawmenubuttons(){
-
+function drawMenuButtons(){
+	if(userMode != 'menu')
+		return;
 	//"play game" button
 	context.fillStyle = "#0000ff";
 	context.fillRect(285, 300, 130, 40);
@@ -34,8 +43,8 @@ function drawmenubuttons(){
 
 //updates the canvas with its (newly moved) shapes
 function redraw(){
-	drawbackground();
-	drawmenubuttons();
+	drawBackground();
+	if(userMode == 'menu') drawMenuButtons();
 }
 
 redraw();
@@ -47,10 +56,32 @@ setInterval(redraw, 100);
 //** -------------- ------------ -------------- **
 
 //handle when the user clicks on the canvas
-context.canvas.addEventListener("click", theyclicked, false);
-function theyclicked(ev){
+context.canvas.addEventListener("click", theyClicked, false);
+
+//decide how to handle click
+function theyClicked(ev){
 	console.log("clickity!");
 	var mouseX = ev.clientX;
 	var mouseY = ev.clientY;
-	alert("You clicked (" + mouseX + ", " + mouseY + ").");
+
+	switch(userMode) {
+		case 'menu':
+			if(!menuClick(ev, mouseX, mouseY))
+				alert("You clicked (" + mouseX + ", " + mouseY + ").");
+			break;
+		default:
+
+			break;
+	}
+}
+
+function menuClick(ev) {
+	if((ev.clientX > 285) && (ev.clientY > 300) && (ev.clientX < 415) && (ev.clientY < 340))
+	{
+		//'play game' has been clicked
+		alert("You clicked the play game button!");
+		return true;
+	} else {
+		return false;
+	}
 }
