7fbc8cdc09a330ee05a3c621b63bbb16a58f9b4c
Lauren <flying.cookies@yahoo.com>
2016-04-12 15:41:28 -0500

    add Button class - drawing only
    the Button class replaces drawing each menu button individually, works
    for drawing but wasClicked is not yet fully implemented or tested

--- game.js
+++ game.js
@@ -8,10 +8,46 @@ context.canvas.height = 600;
 //what to draw and how to treat events; ideas being menu, intro, overworld
 var userMode = 'menu';
 
+//** -------------- -------------- -------------- **
+//** -------------- BUTTONS CLASS? -------------- **
+//** -------------- -------------- -------------- **
+
+class Button{
+	constructor(x, y, width, height, name){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.name = name;
+		console.log("initialized " + this.name);
+	}
+
+	wasClicked(event){
+		if((event.clientX >= x) && (event.clientX <= (x + width)) && (event.clientY >= y) && (event.clientY <= (y + height)))
+			return true;
+		else
+			return false;
+	}
+
+	drawMe(){
+		context.fillStyle = "#0000ff";
+		context.fillRect(this.x, this.y, this.width, this.height);
+		context.font = "20px Arial";
+		context.fillStyle = "#ffffff";
+		context.textAlign = "center";
+		context.textBaseline = "middle";
+		context.fillText(this.name,this.x + (this.width / 2), this.y + (this.height / 2));
+	}
+}
 
 //** -------------- ------------- -------------- **
 //** -------------- DRAWING STUFF -------------- **
 //** -------------- ------------- -------------- **
+
+//** INITIALIZE STUFF TO DRAW ** 
+var titleImage = new Image();
+titleImage.src = "nomekop.png";
+var playGameButton = new Button(285, 300, 130, 40, "play game");
 
 //function to draw the background
 function drawBackground(){
@@ -22,6 +58,8 @@ function drawBackground(){
 			menugradient.addColorStop(1, "white");
 			context.fillStyle = menugradient;
 			context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+			context.drawImage(titleImage, 125, 20);
 			break;
 		case 'intro':
 			context.fillStyle = "#c0ffee";
@@ -35,12 +73,8 @@ function drawBackground(){
 function drawMenuButtons(){
 	if(userMode != 'menu')
 		return;
-	//"play game" button
-	context.fillStyle = "#0000ff";
-	context.fillRect(285, 300, 130, 40);
-	context.font = "20px Arial";
-	context.fillStyle = "#ffffff";
-	context.fillText("play game",305,325);
+
+	playGameButton.drawMe();
 
 }
 
